3df58279c7e59e8dca329ab6c1f053576ffeb067
jona <jonatanduttweiler@gmail.com>
2020-07-06 20:29:56 -0300

    Add config for running ipfs container

---
+++ ipfs/update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+docker exec efem-ipfs ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
+docker exec efem-ipfs ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "GET", "POST"]'
+docker exec efem-ipfs ipfs config --json Gateway.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "GET", "POST", "OPTIONS"]'
+docker exec efem-ipfs ipfs config --json Gateway.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
+docker exec efem-ipfs ipfs config --json Gateway.HTTPHeaders.Access-Control-Expose-Headers '["Ipfs-Hash"]'
+docker exec efem-ipfs ipfs config --json Gateway.Writable true
+
