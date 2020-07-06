afe0be481c1658cdf92bf74efe38c2a07bfd6d56
jona <jonatanduttweiler@gmail.com>
2020-07-06 19:06:39 -0300

    Enable websocket connection on rsk node

--- rsk/run.sh
+++ rsk/run.sh
@@ -3,6 +3,7 @@
 docker run -d \
            --name efem-rsk  \
            -p 4444:4444 \
+           -p 4445:4445\
            -p 30305:30305 \
            --network efem-network \
            regtest
