34ef3a4505d229ec4f8ca0aa5f120159e6cb6178
jona <jonatanduttweiler@gmail.com>
2020-07-06 21:16:00 -0300

    Add account on feathers whitelist

--- connect-containers.sh
+++ connect-containers.sh
@@ -1,3 +1,13 @@
-docker network connect efem-network efem-dapp
-docker network connect efem-network efem-feathers
-docker network connect efem-network mongo-db
+#!/bin/bash
+
+NETWORK=efem-network
+
+#docker network create ${NETWORK}
+
+docker network connect ${NETWORK} efem-dapp
+docker network connect ${NETWORK} efem-feathers
+docker network connect ${NETWORK} mongo-db
+docker network connect ${NETWORK} efem-rsk
+docker network connect ${NETWORK} efem-ipfs
+
+
