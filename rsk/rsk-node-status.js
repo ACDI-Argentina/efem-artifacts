ee1633cd431de3c07c9c40961465812f16567564
cmauriciocoronel <cmauriciocoronel@gmail.com>
2020-07-20 15:40:36 -0300

    Actualización de configuración de dirección de crowdfunding smasrt contract + especificación de ID de red en RSK Regtest.

--- rsk/rsk-node-status.js
+++ rsk/rsk-node-status.js
@@ -9,17 +9,22 @@ console.log('-------------------------------------------');
 var web3 = new Web3(network);
 
 web3.eth.net.getId().then(id => {
-    console.log('Network Id ' + id);
+    console.log('  Network Id ' + id);
 });
 
 web3.eth.net.getNetworkType().then(networkType => {
-    console.log('Network Type ' + networkType);
+    console.log('  Network Type ' + networkType);
 });
 
 web3.eth.getAccounts().then(accounts => {
-    console.log('Accounts');
-    console.log(accounts);
+    console.log('  Accounts balances');
+    accounts.forEach(account => {
+        web3.eth.getBalance(account).then(balance => {
+            console.log(`  - Balance ${account}: ${balance}`);
+        })
+    });
 });
+
 /*
 web3.eth.getBlockNumber().then(blockNumber => {
     console.log('Block Number: ' + blockNumber);
@@ -34,9 +39,9 @@ web3.eth.getAccounts().then(accounts => {
     });
 });*/
 
-let contractAddress = '0xAfCdFA78A5B7e86B317F7DbF95aB33A7aC920F77'
+/*let contractAddress = '0xAfCdFA78A5B7e86B317F7DbF95aB33A7aC920F77'
 let index = 0
 //console.log(web3.eth.getStorageAt(contractAddress, index))
 web3.eth.getStorageAt(contractAddress, index).then(s => {
     console.log(s);
-});
+});*/
