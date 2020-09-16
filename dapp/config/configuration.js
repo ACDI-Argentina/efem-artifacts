80442eb10e58aee8eb35b1920646922c1510a8f6
jona <jonatanduttweiler@gmail.com>
2020-09-16 18:53:15 -0300

    Update dapp configuration
    Merge with i18n new options
    Fix read ipfsPinningEnabled env var

--- dapp/config/configuration.js
+++ dapp/config/configuration.js
@@ -27,7 +27,7 @@ const configurations = {
       ]
     },
     title: 'localhost',
-    crowdfundingAddress: '0xC1007078659F3814607eB7883d6bfc3902212c86',
+    crowdfundingAddress: '0x05A55E87d40572ea0F9e9D37079FB9cA11bdCc67',
     liquidPledgingAddress: '0x46579394802b5e4d2C0647436BFcc71A2d9E8478',
     lppCampaignFactoryAddress: '0xe3155F7A49897e7860476b5A625B258ebe43cA98',
     lppCappedMilestoneFactoryAddress: '0x1b6E4a9eB8264E46784a782c87e3529E203425Ca',
@@ -72,7 +72,7 @@ const configurations = {
       ]
     },
     title: 'RSK Testnet',
-    crowdfundingAddress: '0xC1007078659F3814607eB7883d6bfc3902212c86',
+    crowdfundingAddress: '0x05A55E87d40572ea0F9e9D37079FB9cA11bdCc67',
     liquidPledgingAddress: '0x581A2751C29F030730c99f9435c5f34A82BF4969',
     lppCampaignFactoryAddress: '0xcb5eea43731E1058e5c8FBc989CB2E221602Fb67',
     lppCappedMilestoneFactoryAddress: '0x43E3fC1f59C367b34Cab072AFb2dFE8CEA1CBAFa',
@@ -108,7 +108,7 @@ const configurations = {
         ]
       },
       title: 'RSK Testnet',
-      crowdfundingAddress: '0xC1007078659F3814607eB7883d6bfc3902212c86',
+      crowdfundingAddress: '0x05A55E87d40572ea0F9e9D37079FB9cA11bdCc67',
       liquidPledgingAddress: '0x581A2751C29F030730c99f9435c5f34A82BF4969',
       lppCampaignFactoryAddress: '0xcb5eea43731E1058e5c8FBc989CB2E221602Fb67',
       lppCappedMilestoneFactoryAddress: '0x43E3fC1f59C367b34Cab072AFb2dFE8CEA1CBAFa',
@@ -142,75 +142,67 @@ const configurations = {
         { key: "en", name: "English", flag: "US" }
       ]
     },
-    rsk_mainnet: {
-      language: {
-        default: 'es',
-        options: [
-          { key: "es", name: "Español" },
-          { key: "en", name: "English" }
-        ]
-      },
-      title: 'RSK MainNet',
-      crowdfundingAddress: '0xC1007078659F3814607eB7883d6bfc3902212c86',
-      liquidPledgingAddress: '0x86Fd7661114Ca0Cf959337CB7DAFbBE93dB248d2',
-      lppCampaignFactoryAddress: '0xB874E4B1F4DBEEBCBdB8150EA8c71c3E96FCb40E',
-      lppCappedMilestoneFactoryAddress: '0x95655dC505233d40e2c5A1d4590d142C8a721cb4',
-      nodeConnection: 'https://node.b4h.world',
-      networkName: 'rsk_mainnet',
-      nodeId: 30,
-      etherscan: 'https://explorer.rsk.co/',
-      feathersConnection: 'https://feathers.b4h.world',
-      ipfsGateway: 'https://ipfs.b4h.world/ipfs/',
-      ipfsPinningEnabled: true,
-      sendErrors: true,
-      analytics: {
-        ga_UA: 'UA-136337883-1',
-        useGoogleAnalytics: true,
-        useHotjar: false,
-      },
-      nativeTokenName: 'RBTC',
-      nativeToken: {
-        name: 'RBTC',
-        symbol: 'RBTC',
-        logoCid: '/ipfs/QmTsctkHWeVcuz6z2AWdi5zv3YCcVWSbmg2XF3tVs8jfn1',
-        showDecimals: 4
-      }
+    title: 'RSK MainNet',
+    crowdfundingAddress: '0x05A55E87d40572ea0F9e9D37079FB9cA11bdCc67',
+    liquidPledgingAddress: '0x86Fd7661114Ca0Cf959337CB7DAFbBE93dB248d2',
+    lppCampaignFactoryAddress: '0xB874E4B1F4DBEEBCBdB8150EA8c71c3E96FCb40E',
+    lppCappedMilestoneFactoryAddress: '0x95655dC505233d40e2c5A1d4590d142C8a721cb4',
+    nodeConnection: 'https://node.b4h.world',
+    networkName: 'rsk_mainnet',
+    nodeId: 30,
+    etherscan: 'https://explorer.rsk.co/',
+    feathersConnection: 'https://feathers.b4h.world',
+    ipfsGateway: 'https://ipfs.b4h.world/ipfs/',
+    ipfsPinningEnabled: true,
+    sendErrors: true,
+    analytics: {
+      ga_UA: 'UA-136337883-1',
+      useGoogleAnalytics: true,
+      useHotjar: false,
     },
-  };
+    nativeTokenName: 'RBTC',
+    nativeToken: {
+      name: 'RBTC',
+      symbol: 'RBTC',
+      logoCid: '/ipfs/QmTsctkHWeVcuz6z2AWdi5zv3YCcVWSbmg2XF3tVs8jfn1',
+      showDecimals: 4
+    }
+  },
+};
 
-  // Unknown environment
-  if (configurations[REACT_APP_ENVIRONMENT] === undefined)
-    throw new Error(
-      `There is no configuration object for environment: ${REACT_APP_ENVIRONMENT}. Expected REACT_APP_ENVIRONMENT to be empty or one of: ${Object.keys(
-        configurations,
-      )}`,
-    );
-  
-  // Create config object based on environment setup
-  const config = Object.assign({}, configurations[REACT_APP_ENVIRONMENT]);
-  
-  // Overwrite the environment values with parameters
-  config.crowdfundingAddress = REACT_APP_CROWDFUNDING_ADDRESS || config.crowdfundingAddress;
-  config.liquidPledgingAddress = REACT_APP_LIQUIDPLEDGING_ADDRESS || config.liquidPledgingAddress;
-  config.campaignFactoryAddress =
-    REACT_APP_CAMPAIGN_FACTORY_ADDRESS || config.lppCampaignFactoryAddress;
-  config.cappedMilestoneFactoryAddress =
-    REACT_APP_CAPPED_MILESTONE_FACTORY_ADDRESS || config.lppCappedMilestoneFactoryAddress;
-  config.tokenAddresses = REACT_APP_TOKEN_ADDRESSES
-    ? JSON.parse(REACT_APP_TOKEN_ADDRESSES)
-    : config.tokenAddresses;
-  config.etherscan = REACT_APP_BLOCKEXPLORER || config.etherscan;
-  config.feathersConnection = REACT_APP_FEATHERJS_CONNECTION_URL || config.feathersConnection;
-  config.nodeConnection = REACT_APP_NODE_CONNECTION_URL || config.nodeConnection;
-  config.decimals = REACT_APP_DECIMALS;
-  config.bugsEmail = REACT_APP_BUGS_EMAIL;
-  config.networkName = REACT_APP_NETWORK_NAME || config.networkName;
-  config.nodeId = (REACT_APP_NODE_ID && Number.parseInt(REACT_APP_NODE_ID, 10)) || config.nodeId;
-  config.nativeTokenName = REACT_APP_NATIVE_TOKEN_NAME || config.nativeTokenName;
+// Unknown environment
+if (configurations[REACT_APP_ENVIRONMENT] === undefined)
+  throw new Error(
+    `There is no configuration object for environment: ${REACT_APP_ENVIRONMENT}. Expected REACT_APP_ENVIRONMENT to be empty or one of: ${Object.keys(
+      configurations,
+    )}`,
+  );
 
-  config.ipfsGateway = REACT_APP_IPFS_GATEWAY || config.ipfsGateway;
-  config.ipfsPinningEnabled = (REACT_APP_IPFS_PINNING_ENABLED !== undefined)? REACT_APP_IPFS_PINNING_ENABLED : config.ipfsPinningEnabled;
-  
-  //config.sendErrors = ['develop', 'release', 'beta', 'rsk_testnet'].includes(REACT_APP_ENVIRONMENT);
-  console.log(config);
-  export default config;
+// Create config object based on environment setup
+const config = Object.assign({}, configurations[REACT_APP_ENVIRONMENT]);
+
+// Overwrite the environment values with parameters
+config.crowdfundingAddress = REACT_APP_CROWDFUNDING_ADDRESS || config.crowdfundingAddress;
+config.liquidPledgingAddress = REACT_APP_LIQUIDPLEDGING_ADDRESS || config.liquidPledgingAddress;
+config.campaignFactoryAddress =
+  REACT_APP_CAMPAIGN_FACTORY_ADDRESS || config.lppCampaignFactoryAddress;
+config.cappedMilestoneFactoryAddress =
+  REACT_APP_CAPPED_MILESTONE_FACTORY_ADDRESS || config.lppCappedMilestoneFactoryAddress;
+config.tokenAddresses = REACT_APP_TOKEN_ADDRESSES
+  ? JSON.parse(REACT_APP_TOKEN_ADDRESSES)
+  : config.tokenAddresses;
+config.etherscan = REACT_APP_BLOCKEXPLORER || config.etherscan;
+config.feathersConnection = REACT_APP_FEATHERJS_CONNECTION_URL || config.feathersConnection;
+config.nodeConnection = REACT_APP_NODE_CONNECTION_URL || config.nodeConnection;
+config.decimals = REACT_APP_DECIMALS;
+config.bugsEmail = REACT_APP_BUGS_EMAIL;
+config.networkName = REACT_APP_NETWORK_NAME || config.networkName;
+config.nodeId = (REACT_APP_NODE_ID && Number.parseInt(REACT_APP_NODE_ID, 10)) || config.nodeId;
+config.nativeTokenName = REACT_APP_NATIVE_TOKEN_NAME || config.nativeTokenName;
+
+config.ipfsGateway = REACT_APP_IPFS_GATEWAY || config.ipfsGateway;
+config.ipfsPinningEnabled = (REACT_APP_IPFS_PINNING_ENABLED !== undefined) ? REACT_APP_IPFS_PINNING_ENABLED : config.ipfsPinningEnabled;
+
+//config.sendErrors = ['develop', 'release', 'beta', 'rsk_testnet'].includes(REACT_APP_ENVIRONMENT);
+console.log(config);
+export default config;
