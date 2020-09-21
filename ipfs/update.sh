e9c4f6c6511189c5b80c9b62ea491b51eb39c55d
jona <jonatanduttweiler@gmail.com>
2020-09-20 21:21:21 -0300

    Fix ipfs clear-site-data

--- ipfs/update.sh
+++ ipfs/update.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+docker exec efem-ipfs ipfs config --json API.HTTPHeaders.Access-Control-Allow-Credentials '["true"]'
 docker exec efem-ipfs ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
 docker exec efem-ipfs ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "GET", "POST"]'
+docker exec efem-ipfs ipfs config --json Gateway.HTTPHeaders.Access-Control-Allow-Credentials '["true"]'
 docker exec efem-ipfs ipfs config --json Gateway.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
 docker exec efem-ipfs ipfs config --json Gateway.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "GET", "POST", "OPTIONS"]'
 docker exec efem-ipfs ipfs config --json Gateway.HTTPHeaders.Access-Control-Expose-Headers '["Ipfs-Hash"]'
